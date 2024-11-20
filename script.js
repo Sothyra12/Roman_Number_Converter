333ae4803ed4d3bb954acd4563c5b0af8d8370f8
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-11-19 22:36:40 -0500

    used for..of to loop through the objects with the same function calling within until met the desired conditions

--- script.js
+++ script.js
@@ -3,54 +3,58 @@
 const btn = document.getElementById("convert-btn");
 const inputVal = document.getElementById("number");
 const outputVal = document.getElementById("output");
+const romanNum = {
+  1000: "M",
+  900: "CM",
+  500: "D",
+  400: "CD",
+  100: "C",
+  90: "XC",
+  50: "L",
+  40: "XL",
+  10: "X",
+  9: "IX",
+  5: "V",
+  4: "IV",
+  1: "I",
+};
+
+const objKeys = Object.keys(romanNum)
+  .map(Number)
+  .sort((a, b) => b - a);
 
 const integerToRoman = (input) => {
-  if (input === 0) {
-    return "";
+  if (input === 0) return "";
+
+  for (let objKey of objKeys) {
+    if (input >= objKey) {
+      // romanNum[objKey] returns the value of the key in the object e.g. romanNum[1000] returns "M"
+      return romanNum[objKey] + integerToRoman(input - objKey);
+    }
   }
-  if (input >= 1000) {
-    return "M" + integerToRoman(input - 1000);
-  } else if (input >= 900) {
-    return "CM" + integerToRoman(input - 900);
-  } else if (input >= 500) {
-    return "D" + integerToRoman(input - 500); 
-  } else if (input >= 400) {
-    return "CD" + integerToRoman(input - 400); 
-  } else if (input >= 100) {
-    return "C" + integerToRoman(input - 100); 
-  }else if (input >= 90) {
-    return "XC" + integerToRoman(input - 90); 
-  } else if (input >= 50) {
-    return "L" + integerToRoman(input - 50); 
-  } else if (input >= 40) {
-    return "XL" + integerToRoman(input - 40); 
-  } else if (input >= 10) {
-    return "X" + integerToRoman(input - 10); 
-  } else if (input >= 9) {
-    return "IX" + integerToRoman(input - 9); 
-  } else if (input >= 5) {
-    return "V" + integerToRoman(input - 5); 
-  } else if (input >= 4) {
-    return "IV" + integerToRoman(input - 4); 
-  } else {
-    return "I" + integerToRoman(input - 1);
-  } 
+
+  // if (input >= 1000) {
+  //   return "M" + integerToRoman(input - 1000);
+  // } else {
+  //   return "I" + integerToRoman(input - 1);
+  // }
 };
 
 const checkUserInput = () => {
+  const inputStrToNum = parseInt(inputVal.value);
   switch (true) {
-  case !inputVal.value:
-    outputVal.innerText = "Please enter a valid number";
-    break;
-  case inputVal.value <= 1:
-    outputVal.innerText = "Please enter a number greater than or equal to 1";
-    break;
-  case inputVal.value >= 4000:
-    outputVal.innerText = "Please enter a number less than or equal to 3999";
-    break;
-  default:
-    outputVal.textContent = integerToRoman(inputVal.value);
-}
+    case !inputVal.value:
+      outputVal.innerText = "Please enter a valid number";
+      break;
+    case inputVal.value < 1:
+      outputVal.innerText = "Please enter a number greater than or equal to 1";
+      break;
+    case inputVal.value >= 4000:
+      outputVal.innerText = "Please enter a number less than or equal to 3999";
+      break;
+    default:
+      outputVal.textContent = integerToRoman(inputStrToNum);
+  }
   inputVal.value = "";
 };
 
@@ -61,4 +65,3 @@ inputVal.addEventListener("keydown", (e) => {
     checkUserInput();
   }
 });
-
